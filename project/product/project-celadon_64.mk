8f5f931a9ac4d85cae68dff812d82d6fdbdfd52f
swei22 <w.sheng@intel.com>
2018-04-10 16:55:04 +0800

    Add new project project-celadon_64 for trusty
    Create file project/product/project-celadon_64.mk
    
    Signed-off-by: swei22 <w.sheng@intel.com>

---
+++ project/product/project-celadon_64.mk
@@ -0,0 +1,18 @@
+# compiled from source
+TRUSTY_ALL_USER_TASKS += \
+        sand/hwcrypto \
+        keymaster \
+        storage
+
+# TAs for debug build
+ifeq ($(LKDEBUG), 2)
+TRUSTY_ALL_USER_TASKS += \
+        sand/benchmark \
+        sample/hwcrypto-unittest \
+        sample/storage-unittest \
+        sample/ipc-unittest/main \
+        sample/ipc-unittest/srv
+
+endif
+
+GLOBAL_DEFINES += PRINT_USE_MMIO=1
