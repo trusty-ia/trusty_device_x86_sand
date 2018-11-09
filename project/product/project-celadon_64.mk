fab7b82e5a2f066f10958603849eba5f54e9583f
Xu,Min <min.m.xu@intel.com>
2018-11-09 15:47:50 +0800

    Trusty - Enable unittest

--- project/product/project-celadon_64.mk
+++ project/product/project-celadon_64.mk
@@ -16,5 +16,12 @@ TRUSTY_ALL_USER_TASKS += \
 
 endif
 
+TRUSTY_ALL_USER_TASKS += \
+        sand/benchmark \
+        sample/hwcrypto-unittest \
+        sample/storage-unittest \
+        sample/ipc-unittest/main \
+        sample/ipc-unittest/srv
+
 GLOBAL_DEFINES += PRINT_USE_MMIO=0
 GLOBAL_DEFINES += APP_STORAGE_RPMB_BLOCK_COUNT=8192
