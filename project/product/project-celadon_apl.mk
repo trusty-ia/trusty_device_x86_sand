2f8bb95e421cbeb8a3da5b4e4de1d2dfb2384020
sheng wei <w.sheng@intel.com>
2019-01-18 10:44:27 +0800

    Add project special files for celadon apl and kbl
    Add project-celadon_apl.mk for APL NUC
    Add project-celadon_kbl.mk for Commercial KBL NUC
    
    Signed-off-by: sheng wei <w.sheng@intel.com>

---
+++ project/product/project-celadon_apl.mk
@@ -0,0 +1,21 @@
+# compiled from source
+TRUSTY_ALL_USER_TASKS += \
+        sand/hwcrypto \
+        keymaster \
+        gatekeeper \
+        storage
+
+# TAs for debug build
+ifeq ($(LKDEBUG), 2)
+TRUSTY_ALL_USER_TASKS += \
+        sand/benchmark \
+        sample/hwcrypto-unittest \
+        sample/storage-unittest \
+        sample/ipc-unittest/main \
+        sample/ipc-unittest/srv
+
+endif
+
+GLOBAL_DEFINES += PRINT_USE_MMIO=0
+GLOBAL_DEFINES += APP_STORAGE_RPMB_BLOCK_COUNT=8192
+GLOBAL_DEFINES += APP_STORAGE_RPMB_OFFSET=512
